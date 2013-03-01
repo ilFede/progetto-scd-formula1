bfdf780087b6fc62e4a884baa1246ffc559f3943
federico.cacco <federico.cacco@c1f4f423-dd15-7dac-07f1-bfb3a2939412>
2013-03-01 11:19:25 +0000

    Continuato sviluppo dei piloti

--- Workspace/src/pilot/pilot.ads
+++ Workspace/src/pilot/pilot.ads
@@ -2,6 +2,8 @@ with Common;
 with Race;
 with Car;
 with Ada.Numerics.Generic_Elementary_Functions;
+with Ada.Containers;
+with Ada.Containers.Vectors;
 
 package Pilot is
    use Common;
@@ -11,9 +13,14 @@ package Pilot is
    -- task per descrivere il pilota
    task type Pilot_T (Pilot_Config_File_Ref : String_Ref_T;
 		      Car_Config_File_Ref : String_Ref_T;
-		      Stategy          : Integer);
+		      Stategy          : Float_Vector_Ref_T);
 
-   -- tipo per l'accesso a una variabile di tipo Car_T
+   -- tipo per l'accesso a una variabile di tipo Pilot_T
    type Pilot_Ref_T is access Pilot_T;
+
+   -- vettore per i piloti
+   package Pilot_Vector_T is new Ada.Containers.Vectors
+     (Element_Type => Pilot_Ref_T,
+      Index_Type   => Natural);
 
 end Pilot;
